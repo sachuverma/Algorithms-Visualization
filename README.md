0acda3a49544c97ef8c705bda0c24d4edf504674
sachinverma53121 <sachinverma53121@gmail.com>
2020-09-17 16:44:01 +0530

    edits

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# Floodfill-Algo-Visualization
+# Maze Paths Algorithm Visualization
 
-A simple visualization of *Backtracking* code to find all the maze paths.
-[View Here](https://sachinverma53121.github.io/Mazepath-Algo-Visualization/)
+## A simple visualization of **backtracking** code [Check Here](https://sachinverma53121.github.io/Mazepath-Algo-Visualization/)
+
+### I tried making a visualization for a data structures problem where _we need to find all possible paths from top left cell to bottom right cell in a maze_.
+
+### I saw this [problem](https://lnkd.in/g4kYp8y) on Pepcoding resourses page.
+
+## Approach:
+
+The algorithm used here is backtracking.  
+We recursively call for next step in top, left, down, right of current cell and visit it if it's inside maze and is not an obstacle.  
+This will follow a path and check if the path reaches the destination or not. If the path does not reach the destination then backtrack and try other paths.
+
 ![1-Maze Path Animation](demo/maze_path.gif)
--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 // Global variables
-// Array to keep trak of visited cells so that we don't form endless loops with traking
-let visited = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
 // A random 10x10 maze where 0 denotes vacent cell and 1 denotes obstacle
 let maze = [
   [0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -27,6 +13,11 @@ let maze = [
   [0, 1, 0, 0, 0, 1, 1, 0, 0, 0],
 ];
 
+// Array to keep trak of visited cells so that we don't form endless loops with traking
+let visited = Array(maze.length)
+  .fill()
+  .map(() => Array(maze[0].length).fill(0));
+
 // maze styles based on maze matrix
 let mazeBody = document.getElementById("maze-body");
 mazeBody.style.width = (maze[0].length + 1) * 50 + 10 + "px";
@@ -34,9 +25,9 @@ let br = document.createElement("br");
 let startButton = document.getElementById("start-button");
 let pathList = document.getElementById("paths-list");
 
-let r = maze.length - 1;                                      // max rows in maze
-let c = maze[0].length - 1;                                   // max columns in maze
-let pathCount = 0;                                            // valid paths counter
+let r = maze.length - 1; // max rows in maze
+let c = maze[0].length - 1; // max columns in maze
+let pathCount = 0; // valid paths counter
 
 // Displaying maze on front-end based on the maze matrix earlier initialized
 for (let i = 0; i < maze.length; ++i) {
@@ -51,27 +42,29 @@ for (let i = 0; i < maze.length; ++i) {
 // adding a different color to destination cell
 document.getElementById(r + "_" + c).classList.add("last-square");
 
-
 // start function ran on clicking start button
 let start = async () => {
   console.log("starting flood fill");
   startButton.innerHTML = "Running";
+  startButton.classList.add("running-button");
   startButton.disabled = true;
+  startButton.style.opacity = 0.7;
   resetVisited();
-  
+
   // calling the funtion having backtracking algorithm
-  await getMazePath(maze, 0, 0, ""); 
-  
+  await getMazePath(maze, 0, 0, "");
+
   console.log("ended flood fill");
-  startButton.innerHTML = "Start Floodfill";
+  startButton.innerHTML = "Start Algorithm";
+  startButton.classList.remove("running-button");
   startButton.disabled = false;
+  startButton.style.opacity = 1;
 };
 
-
-// =============== Backtracking function brgin ================ //
-// function having backtracking algorithm to find all valid paths
+// =============== Backtracking function ================ //
 let getMazePath = async (maze, r, c, ans) => {
-  if (  
+  // checking if current cell is valid
+  if (
     r < 0 ||
     c < 0 ||
     r >= maze.length ||
@@ -79,25 +72,26 @@ let getMazePath = async (maze, r, c, ans) => {
     maze[r][c] == 1 ||
     visited[r][c] == 1
   )
-    return;                                                 // return to try another path if cell is invalid or we can't proceed
+    return; // return to try another path if cell is invalid or we can't proceed
 
-  if (r == maze.length - 1 && c == maze[0].length - 1) {    // cell is the destination cell
+  // cell is the destination cell
+  if (r == maze.length - 1 && c == maze[0].length - 1) {
     pathCount++;
     // updating path count on front-end
     document.getElementById("path-display").innerHTML =
       pathCount + " paths found";
-    
+
     // appending the valid path pattern to the list
     let li = document.createElement("li");
     li.textContent = ans;
     pathList.appendChild(li);
 
-    console.log("Path is: '" + ans + "'");
+    console.log("Path " + pathCount + ": '" + ans + "'");
     document.getElementById(r + "_" + c).classList.add("found-path");
     await sleep(150);
     document.getElementById(r + "_" + c).classList.remove("found-path");
 
-    return;                                                // returning back to try other paths also
+    return; // returning back to try other paths also
   }
 
   let currSq = document.getElementById(r + "_" + c);
@@ -105,10 +99,11 @@ let getMazePath = async (maze, r, c, ans) => {
   await sleep(150);
   visited[r][c] = 1;
 
-  await getMazePath(maze, r - 1, c, ans + "t");           // calling to top cell to current cell
-  await getMazePath(maze, r, c - 1, ans + "l");           // calling to left cell to current cell
-  await getMazePath(maze, r + 1, c, ans + "d");           // calling to down cell to current cell
-  await getMazePath(maze, r, c + 1, ans + "r");           // calling to right cell to current cell
+  // making calls recursively without checking
+  await getMazePath(maze, r - 1, c, ans + "t"); // calling to top cell to current cell
+  await getMazePath(maze, r, c - 1, ans + "l"); // calling to left cell to current cell
+  await getMazePath(maze, r + 1, c, ans + "d"); // calling to down cell to current cell
+  await getMazePath(maze, r, c + 1, ans + "r"); // calling to right cell to current cell
 
   visited[r][c] = 0;
   currSq.classList.remove("visited-square");
@@ -116,22 +111,16 @@ let getMazePath = async (maze, r, c, ans) => {
 };
 // =============== Backtracking function end ================ //
 
-
-function resetVisited() {                                 // reset the array which tracks visited cells at beginning
-  visited = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
+// =============== Some Helping Functions =================== //
+function resetVisited() {
+  // reset the array which tracks visited cells at beginning
+  visited = Array(maze.length)
+    .fill()
+    .map(() => Array(maze[0].length).fill(0));
 }
 
-function sleep(ms) {                                     // sleep function to have some delay in algorithm to visualize more effectively
+function sleep(ms) {
+  // sleep function to have some delay in algorithm to visualize more effectively
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+// =============== End ================= //
